04b6a59d177f2054e5b0d386cdf127be13ab874e
Emmmmm1r <fjbou0@gmail.com>
2023-08-06 01:07:53 +0300

    HW6

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,26 @@
-import React, {useState} from 'react'
+import React from 'react';
+import './App.css'
+import Slider from './components/Slider';
+import Image1 from './assets/Slider_1.jpg'
+import Image2 from './assets/Slider_2.jpg'
+import Image3 from './assets/Slider_3.jpg'
+import Image4 from './assets/Slider_4.jpg'
 
-export default function App() {
-    const [result, setResult] = useState('no data')
-    const [bill, setBill] = useState('')
-    const [guests, setGuests] = useState('')
-    let moneyPerGuest = null
 
-    const calculate = () => {
-        if (bill.trim() === '' || guests.trim() === '') {
-            alert('Not enough data, please check your introduced data.')
-        } else {
-            moneyPerGuest = bill / guests
-            setResult(Number(Math.round(moneyPerGuest)))
-            setBill('')
-            setGuests('')
-        }
-    }
+const photos = [
+    Image1,
+    Image2,
+    Image3,
+    Image4,
+]
 
+const App = () => {
     return (
-        <div className={'main'}>
-            <h1>Bill Calculator</h1>
-            <input type="text" placeholder={'Bill amount...'} value={bill} onChange={(e) => setBill(e.target.value)}/>
-            <input type="text" placeholder={'Amount of guests...'} value={guests} onChange={(e) => setGuests(e.target.value)}/>
-            <div className={'result'}>Every guest pays:   ~{result}</div>
-            <button onClick={calculate}>Calculate!</button>
+        <div className={'wrapper'}>
+            <h1>Мой фото-слайдер</h1>
+            <Slider photos={photos}/>
         </div>
     )
 }
+
+export default App
